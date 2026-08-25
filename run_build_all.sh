970a65653ad802f77c302d60373d339d505ecb04
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2026-08-25 21:16:45 +0200

    run_build_all.sh

---
+++ run_build_all.sh
@@ -0,0 +1,10 @@
+set -euox pipefail
+
+. .venv/bin/activate
+
+python -m antennenvergleich.run_0_s1p
+python -m antennen.mazzoni_baby_loop_HB0SM.h_field.h_field_data
+python -m antennen.mazzoni_midi_loop_HB0SM.h_field.h_field_data
+python -m antennenvergleich.run_2_html
+
+uv build --wheel
