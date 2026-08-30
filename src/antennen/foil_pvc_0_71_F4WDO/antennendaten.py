2d150ff4c805a31f0cd7e7ebdc06010c7f0fc5aa
Peter Maerki <github.com.public.peter@ergoinfo.ch>
2026-08-30 20:06:07 +0200

    entwurf

---
+++ src/antennen/foil_pvc_0_71_F4WDO/antennendaten.py
@@ -0,0 +1,39 @@
+from antennenvergleich.datatypes import (
+    Antenna,
+    FloatText,
+    IntText,
+    VnaCalibration,
+)
+
+from math import pi, sqrt
+
+"""Achteck"""
+
+_AREA_FREECAD_M2 = 0.41761
+
+ANTENNENDATEN = Antenna(
+    color="#5d363b",  # color from compare_colors.py
+    selection_brand="Selfmade",
+    selection_location="F4WDO",
+    selection_name="foil_pvc_0_71",
+    D_m=FloatText(
+        2 * sqrt(_AREA_FREECAD_M2 / pi),
+        f"äquiv. Diameter = 2*sqrt(A/pi) mit A = {_AREA_FREECAD_M2:0.4f} m^2 from drawing Freecad.",
+    ),
+    d_m=FloatText(0.045, "Scetch: Tube Diameter 45 mm."),
+    n=IntText(1, ""),
+    p_m=FloatText(0.0, ""),
+    vna_calibration=VnaCalibration.AT_VNA,
+    powerP_W=FloatText(30.0, "Unclear, todo."),
+    info_str="Mag loop made of copper foil glued to PVC pipe, with an air-variable capacitor.",
+    overview_pictures=("images/loopzoom_f4wdo_loop_2a.jpg",),
+    info_enviroment_str="In the attic, in a wooden structure with brick walls, 1.86 m above floor.",
+    info_conductor_str="Copper foil  0.1 mm glued to a 45 mm PVC pipe. Joints were soldered with soft solder.",
+    info_capacitor_str="Air-variable capacitor, 4 mm spacing, 10-175 pF, unknown type or brand.",
+    info_thanks_str="Many thanks to Richard F4WDO for the measurements, drawings, and emails.",
+    enviroment_html="enviroment.html",
+    antenna_build_html="antenna_build.html",
+    measurement_html="measurement.html",
+    final_remarks_html="final_remarks.html",
+    bands=[],
+)
